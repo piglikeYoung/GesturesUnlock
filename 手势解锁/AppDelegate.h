525d2a848b81a19e565d49c6f91cc71e74ea038f
piglikeyoung <316046510@qq.com>
2015-03-16 09:41:35 +0800

    Initial Commit

---
+++ 手势解锁/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手势解锁
+//
+//  Created by piglikeyoung on 15/3/16.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手势解锁/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手势解锁
+//
+//  Created by piglikeyoung on 15/3/16.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
